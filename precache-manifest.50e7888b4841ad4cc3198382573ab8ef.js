53052b287fd13a2c84853d683f986ad9966d06fc
Neda Jahadi <neda_jahadi@yahoo.com>
2020-05-13 22:18:20 +0200

    Updates

---
+++ precache-manifest.50e7888b4841ad4cc3198382573ab8ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3b3590caf3843f0ac6c943ff4fac31c",
+    "url": "/react-laboration/index.html"
+  },
+  {
+    "revision": "6da4985ba53333d6024e",
+    "url": "/react-laboration/static/css/main.69eaf665.chunk.css"
+  },
+  {
+    "revision": "beb2f3288bc21c36900f",
+    "url": "/react-laboration/static/js/2.33655da2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-laboration/static/js/2.33655da2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6da4985ba53333d6024e",
+    "url": "/react-laboration/static/js/main.2bce309c.chunk.js"
+  },
+  {
+    "revision": "edb5be4d18e49db27241",
+    "url": "/react-laboration/static/js/runtime-main.9733ae1e.js"
+  }
+]);
